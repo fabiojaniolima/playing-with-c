7b9bc346f9d964787728d0b36054917eb6fb09a9
fabiojaniolima <fabiojaniolima@gmail.com>
2020-04-09 14:18:08 -0300

    Changed from tab to space

--- ls/utils/ls.h
+++ ls/utils/ls.h
@@ -1,8 +1,8 @@
-#include <dirent.h>	// Permite obter informações a respeito de arquivos e diretórios
-#include <stdio.h>	// Permite lidar com entrada e saída de dados
+#include <dirent.h> // Permite obter informações a respeito de arquivos e diretórios
+#include <stdio.h>  // Permite lidar com entrada e saída de dados
 #include <string.h> // Facilita a manipulação de cadeia de caracteres
 #include <stdlib.h> // Permite lidar com alocação de memória, controle de processos, conversões e outras
-#include <errno.h>	// Facilita a identificação e anúncio de erros em tempo de execução
+#include <errno.h>  // Facilita a identificação e anúncio de erros em tempo de execução
 #include <locale.h> // Permite definir aspectos de localização (idioma e outros)
 
 /*
